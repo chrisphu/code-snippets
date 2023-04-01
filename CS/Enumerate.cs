85bc7182906fb02ff6b89320b09827af9780aa92
Chris Phu <67293666+chrisphu@users.noreply.github.com>
2023-03-31 21:56:00 -0500

    Update Enumerate.cs

--- CS/Enumerate.cs
+++ CS/Enumerate.cs
@@ -1,17 +1,19 @@
 using System;
 
-/// <summary>
-/// <c>Enumerate</c> returns a tuple array where each tuple contains the array element index and the original array element. This is intended to mirror the enumerate function in Python.
-/// </summary>
 public static class ArrayExtensions
 {
+    /// <summary>
+    /// Static (int, T)[] <c>Enumerate</c> returns a tuple array where each tuple contains: [0] the array element index, and [1] the original array element. This is intended to mirror the enumerate function in Python.
+    /// </summary>
     public static (int, T)[] Enumerate<T>(this T[] input)
     {
         (int, T)[] output = new (int, T)[input.Length];
+        
         for (int i = 0; i < input.Length; i++)
         {
             output[i] = (i, input[i]);
         }
+        
         return output;
     }
 }
